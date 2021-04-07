86864a57294692c71d0194721544d7f3d8d84b5f
Pham Son <swlpham@gmail.com>
2021-04-08 00:38:43 +0700

    disabled cv2 imshow

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-from vectornet.ops import *
+from ops import *
 slim = tf.contrib.slim
 
 def VDSR(x, hidden_num, repeat_num, data_format, use_norm, name='VDSR',
